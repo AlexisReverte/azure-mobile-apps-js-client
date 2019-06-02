a994050b2b6748df3ac8c669e2f50f82549baf6d
Alexis Reverte <alexis@matsiya.com>
2019-06-02 21:37:36 +0200

    Remove cordova reference

--- sdk/src/Platform/sdkExports.js
+++ sdk/src/Platform/sdkExports.js
@@ -2,12 +2,12 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // ----------------------------------------------------------------------------
 
-var target = require('./environment').getTarget();
+// var target = require('./environment').getTarget();
 
-if (target === 'Cordova') {
-    module.exports = require('./cordova/sdkExports');
-} else if (target === 'Web') {
-    module.exports = require('./web/sdkExports');
-} else {
-    throw new Error('Unsupported target');
-}
+// if (target === 'Cordova') {
+//     module.exports = require('./cordova/sdkExports');
+// } else if (target === 'Web') {
+module.exports = require('./web/sdkExports');
+// } else {
+//     throw new Error('Unsupported target');
+// }
